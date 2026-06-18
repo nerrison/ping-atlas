01f23f88968966106e8785384fda5326fa75225e
nerrison <philipnerison26@gmail.com>
2026-06-18 11:24:02 +0000

    update main.py route

--- server/app/main.py
+++ server/app/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from app.db.database import Base,engine
-
+from app.routes import groups
+from app.db.init_db import init_db
 
 app = FastAPI()
 
-
+init_db()
 
 @app.get("/")
 def home():
@@ -15,6 +16,6 @@ def db_test():
     with engine.connect() as connection:
         return {"database": "connected"}
     
-
+app.include_router(groups.router)
     
     
